b8b11c73a18876f0006260eb65bd2bb9da1c5766
Donvine254 <donvinemugendi@gmail.com>
2025-08-10 22:51:41 +0300

    feat(examples): add example3 component
    Replaces Example2 with Example3 in App.tsx for demonstration.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import Example1 from "./components/example1";
-import Example2 from "./components/example2";
+// import Example2 from "./components/example2";
+import Example3 from "./components/example3";
 
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50">
       {/* <Example1 /> */}
-      <Example2 />
+      {/* <Example2 /> */}
+      <Example3 />
     </div>
   );
 }
